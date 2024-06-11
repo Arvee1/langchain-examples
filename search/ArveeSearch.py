3789d8e737ed887eb0394382d705aaf0da6eee08
Arvee1 <160126772+Arvee1@users.noreply.github.com>
2024-06-11 22:50:27 +1000

    Update ArveeSearch.py

--- search/ArveeSearch.py
+++ search/ArveeSearch.py
@@ -2,33 +2,27 @@ import streamlit as st
 from langchain_openai import ChatOpenAI
 from langchain.agents import load_tools, initialize_agent
 
-# Streamlit app
-st.subheader('Wazzup!!! What do you want to Search for?')
-
-# Get OpenAI API key, SERP API key and search query
-# with st.sidebar:
-    # openai_api_key = st.text_input("OpenAI API key", value="", type="password")
-    # st.caption("*If you don't have an OpenAI API key, get it [here](https://platform.openai.com/account/api-keys).*")
-    # serpapi_api_key = st.text_input("SERP API Key", type="password")
-    # st.caption("*If you don't have a SERP API key, get it [here](https://serpapi.com).*")
-
+# Set API keys from session state
 openai_api_key = st.secrets["api_key"]
-serpapi_api_key = st.secrets["serp_api"]
-st.session_state.openai_api_key = openai_api_key
-st.session_state.serper_api_key = serpapi_api_key
-search_query = st.text_input("Search Query")
+serper_api_key = st.secrets["serp_api"]
+
+# Streamlit app
+st.subheader('Wazzupp!!! What do you want to search for?')
+search_query = st.text_input("Enter Search Query")
 
 # If the 'Search' button is clicked
 if st.button("Search"):
     # Validate inputs
-    if not openai_api_key.strip() or not serpapi_api_key.strip() or not search_query.strip():
-        st.error(f"Please provide the missing fields.")
+    if not openai_api_key or not serper_api_key:
+        st.error("Please provide the missing API keys in Settings.")
+    elif not search_query.strip():
+        st.error("Please provide the search query.")
     else:
         try:
             with st.spinner('Please wait...'):
-              # Initialize the OpenAI module, load the SerpApi tool, and run the search query using an agent
+              # Initialize the OpenAI module, load the Google Serper API tool, and run the search query using an agent
               llm = ChatOpenAI(temperature=0, openai_api_key=openai_api_key, verbose=True)
-              tools = load_tools(["serpapi"], llm, serpapi_api_key=serpapi_api_key)
+              tools = load_tools(["google-serper"], llm, serper_api_key=serper_api_key)
               agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
               result = agent.run(search_query)
               st.success(result)
